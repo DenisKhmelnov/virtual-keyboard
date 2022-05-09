8f87b3d9ca65654a8c0fa528ec70bd4c85287dca
Denis Khmelnov <denishmelnov@gmail.com>
2022-05-09 13:30:08 +0500

    feat: add keyboard to DOM

--- script.js
+++ script.js
@@ -15,7 +15,19 @@ const Keyboard = {
     capsLock: false,
   },
 
-  init() {},
+  init() {
+    //Create main object
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
+
+    //Setup main element
+    this.elements.main.classList.add("keyboard");
+    this.elements.keysContainer.classList.add("keyboard_keys");
+
+    //Add to DOM
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
 
   _createKeys() {},
 
