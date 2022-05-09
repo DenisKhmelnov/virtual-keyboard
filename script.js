a7e2fe924c5c8310164af887d63daf65088445ce
Denis Khmelnov <denishmelnov@gmail.com>
2022-05-09 13:35:16 +0500

    feat: implement createIconHTML function

--- script.js
+++ script.js
@@ -29,7 +29,60 @@ const Keyboard = {
     document.body.appendChild(this.elements.main);
   },
 
-  _createKeys() {},
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "backspace",
+      "q",
+      "w",
+      "e",
+      "r",
+      "t",
+      "y",
+      "u",
+      "i",
+      "o",
+      "p",
+      "caps",
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "enter",
+      "done",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+      ",",
+      ".",
+      "?",
+      "space",
+    ];
+
+    //Create HTML for icons
+    const createIconHTML = (icon_name) => {
+      return `<i class="material-icons">${icon_name}</i>`;
+    };
+  },
 
   _triggerEvent(handlerName) {},
 
